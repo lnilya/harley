80e012b1010e48d84bac7d81a302c4ad71352a2d
Ilya Shabanov <ilya@one-extra.de>
2021-12-05 16:47:38 -0700

    fixes in build process and icons

--- index.py
+++ index.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
                       host = settings.EEL_HOST)
     else:
         print('Visit http://%s:%d in your browser to use software' % (settings.EEL_HOST, settings.EEL_PORT))
-        print('Sometimes you need to restart this app a few times and wait a bit on first load.')
+        print('On first run it may take a while for the server to be receptive. So if you can\'t reach the site in your browser wait a little bit and reload.')
+        print('Please report any bugs or suggestions to: http://github.com/lnilya/harley')
         eel.init('build')
         eelutil.emptyTmpFolder(settings.TMP_FOLDER)
         eel.start(port = settings.EEL_PORT,
