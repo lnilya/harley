8c948a6566876a2b7c8d54a1cc4f70d10eb13104
Ilya Shabanov <ilya@one-extra.de>
2022-01-20 21:46:59 -0700

    Extended Display Options by adding slider

--- src/sammie/js/ui/modules/DisplayOptions.tsx
+++ src/sammie/js/ui/modules/DisplayOptions.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode} from "react";
-import {FormControl, FormControlLabel, NativeSelect, Switch, Tooltip} from "@mui/material";
+import {FormControl, FormControlLabel, NativeSelect, Slider, Switch, Tooltip} from "@mui/material";
 import ButtonIcon from "../elements/ButtonIcon";
+import {cl} from "../../util";
 
 interface IDisplayOptionsProps {
     
@@ -8,8 +9,10 @@ interface IDisplayOptionsProps {
     className?: string
     settings?: Array<DisplayOptionSetting<any>>,
     modKeys?: Array<DisplayOptionModKey>,
-    activeModKeys?:string[]
+    /**Either an array corresponding to "name" field of modKeys array, or a record coming from useToggleKeys hook*/
+    activeModKeys?:string[]|Record<string, boolean>
     children?:any,
+    col?:'black'|null
 }
 
 export type DisplayOptionModKey = {
@@ -24,23 +27,33 @@ export type DisplayOptionSetting<T> = {
     value: T,
     /**For dropdown type only, options to display*/
     options?:Record<string, string>
+    /**For sliders only, min,max,step*/
+    sliderParams?:[number,number,number]
 }
 /**
  * DisplayOptions is a board of true/false switches to direct the display of images or anything else.
  * @author Ilya Shabanov
  */
-const DisplayOptions: React.FC<IDisplayOptionsProps> = ({children,activeModKeys, modKeys, className, settings}) => {
+const DisplayOptions: React.FC<IDisplayOptionsProps> = ({col,children,activeModKeys, modKeys, className, settings}) => {
+    
+    if(!Array.isArray(activeModKeys)){
+        activeModKeys = Object.keys(activeModKeys).filter(k=>activeModKeys[k])
+    }
     
     return (
-        <div className={`display-options fl-row-start pad-25-left pad-50-ver ${className}`}>
+        <div className={`display-options fl-row-start pad-25-left pad-50-ver ${className} col-${col||'default'}`}>
             {settings?.map((s, i) => {
                     if(!s.type || s.type == 'binary'){
-                        return <FormControlLabel key={i}
-                                          control={
-                                              <Switch checked={s.value}
-                                                  onChange={(e) => s.setter(e.target.checked)}
-                                                  name={'s' + i} size={'small'} color={s.color || "primary"} />
-                                          } label={s.label} />
+                        return <div className="display-options__binary" key={i}>
+                            <span>{s.label}</span>
+                            <FormControlLabel key={i}
+                                              label={''}
+                                              control={
+                                                  <Switch checked={s.value}
+                                                      onChange={(e) => s.setter(e.target.checked)}
+                                                      name={'s' + i} size={'small'} color={s.color || "primary"} />
+                                              }/>
+                        </div>
                     }else if(s.type == 'dropdown'){
                         return <div key={i} className={'display-options__dropdown'}>
                             <span>{s.label}</span>
@@ -51,6 +64,11 @@ const DisplayOptions: React.FC<IDisplayOptionsProps> = ({children,activeModKeys,
                                         )}
                                     </NativeSelect>
                                 </FormControl>
+                        </div>
+                    }else if(s.type == 'slider'){
+                        return <div key={i} className={'display-options__slider'}>
+                            <span>{s.label}</span>
+                            <Slider valueLabelDisplay={'auto'} size={'small'} value={s.value} min={s.sliderParams[0]} max={s.sliderParams[1]} step={s.sliderParams[2]} onChange={(e, v) =>  s.setter(v as number)}/>
                         </div>
                     }
                 }
@@ -63,9 +81,11 @@ const DisplayOptions: React.FC<IDisplayOptionsProps> = ({children,activeModKeys,
                         <strong>Mod keys:</strong>
                         {modKeys?.map((mc)=> {
                             if(!mc) return null;
+                            //@ts-ignore
+                            const isActive = activeModKeys.indexOf(mc.name) != -1;
                                 return (<Tooltip title={mc.desc} placement={'bottom'} key={mc.name} arrow>
                                     <div
-                                        className={`btnicon ${activeModKeys.indexOf(mc.name) != -1 ? 'active' : ''}`}>{mc.name}</div>
+                                        className={`btnicon ${cl(isActive,'active')}`}>{mc.name}</div>
                                 </Tooltip>)
                             }
                         )}
@@ -76,3 +96,4 @@ const DisplayOptions: React.FC<IDisplayOptionsProps> = ({children,activeModKeys,
     );
 }
 export default DisplayOptions;
+
